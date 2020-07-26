93e6f4c38a172c18aed9f5f82755936cd51a8266
seohasong <tisutoo@gmail.com>
2020-07-26 16:42:33 +0000

    continue previous working

--- SSDG/sw-ssdg.js
+++ SSDG/sw-ssdg.js
@@ -4,9 +4,17 @@
 const cacheName = 'ssdg';
 const offlinePage = '.';
 const files = [offlinePage];
-const neverCacheUrls = [/\/\w+$/];
+const neverCacheUrls = [/\/\w+$/, /\/static\//];
 
-// Install
+
+function checkNeverCacheList(url) {
+	if ( this.match(url) ) {
+		return false;
+	}
+	return true;
+}
+
+
 self.addEventListener('install', function(e) {
 	console.log('SuperPWA service worker installation');
 	e.waitUntil(
@@ -21,7 +29,7 @@ self.addEventListener('install', function(e) {
 	);
 });
 
-// Activate
+
 self.addEventListener('activate', function(e) {
 	console.log('SuperPWA service worker activation');
 	e.waitUntil(
@@ -37,62 +45,56 @@ self.addEventListener('activate', function(e) {
 	return self.clients.claim();
 });
 
-// self.addEventListener('fetch', function(e) {
-	
-// 	// Return if the current request url is in the never cache list
-// 	if ( ! neverCacheUrls.every(checkNeverCacheList, e.request.url) ) {
-// 	  console.log( 'SuperPWA: Current request is excluded from cache.' );
-// 	  return;
-// 	}
-	
-// 	// Return if request url protocal isn't http or https
-// 	if ( ! e.request.url.match(/^(http|https):\/\//i) )
-// 		return;
-	
-// 	// Return if request url is from an external domain.
-// 	if ( new URL(e.request.url).origin !== location.origin )
-// 		return;
-	
-// 	// For POST requests, do not use the cache. Serve offline page if offline.
-// 	if ( e.request.method !== 'GET' ) {
-// 		e.respondWith(
-// 			fetch(e.request).catch( function() {
-// 				return caches.match(offlinePage);
-// 			})
-// 		);
-// 		return;
-// 	}
-	
-// 	// Revving strategy
-// 	if ( e.request.mode === 'navigate' && navigator.onLine ) {
-// 		e.respondWith(
-// 			fetch(e.request).then(function(response) {
-// 				return caches.open(cacheName).then(function(cache) {
-// 					cache.put(e.request, response.clone());
-// 					return response;
-// 				});  
-// 			})
-// 		);
-// 		return;
-// 	}
 
-// 	e.respondWith(
-// 		caches.match(e.request).then(function(response) {
-// 			return response || fetch(e.request).then(function(response) {
-// 				return caches.open(cacheName).then(function(cache) {
-// 					cache.put(e.request, response.clone());
-// 					return response;
-// 				});  
-// 			});
-// 		}).catch(function() {
-// 			return caches.match(offlinePage);
-// 		})
-// 	);
-// });
-
-function checkNeverCacheList(url) {
-	if ( this.match(url) ) {
-		return false;
+self.addEventListener('fetch', function(e) {
+	
+	// Return if the current request url is in the never cache list
+	if ( ! neverCacheUrls.every(checkNeverCacheList, e.request.url) ) {
+	  console.log( 'SuperPWA: Current request is excluded from cache.' );
+	  return;
 	}
-	return true;
-}
+	
+	// Return if request url protocal isn't http or https
+	if ( ! e.request.url.match(/^(http|https):\/\//i) )
+		return;
+	
+	// Return if request url is from an external domain.
+	if ( new URL(e.request.url).origin !== location.origin )
+		return;
+	
+	// For POST requests, do not use the cache. Serve offline page if offline.
+	if ( e.request.method !== 'GET' ) {
+		e.respondWith(
+			fetch(e.request).catch( function() {
+				return caches.match(offlinePage);
+			})
+		);
+		return;
+	}
+	
+	// Revving strategy
+	if ( e.request.mode === 'navigate' && navigator.onLine ) {
+		e.respondWith(
+			fetch(e.request).then(function(response) {
+				return caches.open(cacheName).then(function(cache) {
+					cache.put(e.request, response.clone());
+					return response;
+				});  
+			})
+		);
+		return;
+	}
+
+	e.respondWith(
+		caches.match(e.request).then(function(response) {
+			return response || fetch(e.request).then(function(response) {
+				return caches.open(cacheName).then(function(cache) {
+					cache.put(e.request, response.clone());
+					return response;
+				});  
+			});
+		}).catch(function() {
+			return caches.match(offlinePage);
+		})
+	);
+});
