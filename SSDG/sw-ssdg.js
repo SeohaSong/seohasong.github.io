5b0415f4a7ad3c5c5dee1e07a7dd8721aaf7ef8c
seohasong <tisutoo@gmail.com>
2020-07-26 16:29:49 +0000

    continue previous working

--- SSDG/sw-ssdg.js
+++ SSDG/sw-ssdg.js
@@ -37,61 +37,59 @@ self.addEventListener('activate', function(e) {
 	return self.clients.claim();
 });
 
-// Fetch
-self.addEventListener('fetch', function(e) {
+// self.addEventListener('fetch', function(e) {
 	
-	// Return if the current request url is in the never cache list
-	if ( ! neverCacheUrls.every(checkNeverCacheList, e.request.url) ) {
-	  console.log( 'SuperPWA: Current request is excluded from cache.' );
-	  return;
-	}
+// 	// Return if the current request url is in the never cache list
+// 	if ( ! neverCacheUrls.every(checkNeverCacheList, e.request.url) ) {
+// 	  console.log( 'SuperPWA: Current request is excluded from cache.' );
+// 	  return;
+// 	}
 	
-	// Return if request url protocal isn't http or https
-	if ( ! e.request.url.match(/^(http|https):\/\//i) )
-		return;
+// 	// Return if request url protocal isn't http or https
+// 	if ( ! e.request.url.match(/^(http|https):\/\//i) )
+// 		return;
 	
-	// Return if request url is from an external domain.
-	if ( new URL(e.request.url).origin !== location.origin )
-		return;
+// 	// Return if request url is from an external domain.
+// 	if ( new URL(e.request.url).origin !== location.origin )
+// 		return;
 	
-	// For POST requests, do not use the cache. Serve offline page if offline.
-	if ( e.request.method !== 'GET' ) {
-		e.respondWith(
-			fetch(e.request).catch( function() {
-				return caches.match(offlinePage);
-			})
-		);
-		return;
-	}
+// 	// For POST requests, do not use the cache. Serve offline page if offline.
+// 	if ( e.request.method !== 'GET' ) {
+// 		e.respondWith(
+// 			fetch(e.request).catch( function() {
+// 				return caches.match(offlinePage);
+// 			})
+// 		);
+// 		return;
+// 	}
 	
-	// Revving strategy
-	if ( e.request.mode === 'navigate' && navigator.onLine ) {
-		e.respondWith(
-			fetch(e.request).then(function(response) {
-				return caches.open(cacheName).then(function(cache) {
-					cache.put(e.request, response.clone());
-					return response;
-				});  
-			})
-		);
-		return;
-	}
+// 	// Revving strategy
+// 	if ( e.request.mode === 'navigate' && navigator.onLine ) {
+// 		e.respondWith(
+// 			fetch(e.request).then(function(response) {
+// 				return caches.open(cacheName).then(function(cache) {
+// 					cache.put(e.request, response.clone());
+// 					return response;
+// 				});  
+// 			})
+// 		);
+// 		return;
+// 	}
 
-	e.respondWith(
-		caches.match(e.request).then(function(response) {
-			return response || fetch(e.request).then(function(response) {
-				return caches.open(cacheName).then(function(cache) {
-					cache.put(e.request, response.clone());
-					return response;
-				});  
-			});
-		}).catch(function() {
-			return caches.match(offlinePage);
-		})
-	);
-});
+// 	e.respondWith(
+// 		caches.match(e.request).then(function(response) {
+// 			return response || fetch(e.request).then(function(response) {
+// 				return caches.open(cacheName).then(function(cache) {
+// 					cache.put(e.request, response.clone());
+// 					return response;
+// 				});  
+// 			});
+// 		}).catch(function() {
+// 			return caches.match(offlinePage);
+// 		})
+// 	);
+// });
 
-// Check if current url is in the neverCacheUrls list
 function checkNeverCacheList(url) {
 	if ( this.match(url) ) {
 		return false;
